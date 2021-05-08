dfb32e1523c766e94cc2a6447c444df721a370bd
shaun08 <shaungp8193@gmail.com>
2021-05-08 00:42:09 -0400

    First Commit

---
+++ Makefile
@@ -0,0 +1,33 @@
+#
+# \brief: Makefile to bulid custom GPIO memory device driver for Raspberry Pi 4
+# \author: Shaun Prabhu
+#
+
+BINARY      := RPI_GPIO
+KERNEL      := /lib/modules/$(shell uname -r)/build
+ARCH        := arm
+C_FLAGS     := -Wall
+KMOD_DIR    := $(shell pwd)
+TARGET_PATH := /lib/modules/$(shell uname -r)/kernel/driver/char
+
+OBJECTS := rpi_gpio.o
+
+ccflags-y +=  $(C_FLAGS)
+
+obj-m += $(BINARY).o
+
+$(BINARY)-y := $(OBJECTS)
+
+$(BINARY).ko:
+	make -C $(KERNEL) M=$(KMOD_DIR) modules
+
+install:
+	cp $(BINARY).ko $(TARGET_PATH)
+	depmod -a
+
+uninstall:
+	rm $(TARGET_PATH)/$(BINARY).ko
+	depmod -a
+
+clean:
+	make -C $(KERNEL) M=$(KMOD_DIR) clean
---
+++ test_rpi_gpio/test.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define	BLOCK_SIZE		(4*1024)
+
+static volatile unsigned int GPIO_BASE = 0x00200000;
+static volatile unsigned int *gpio = NULL;
+
+int main()
+{
+  int fd = open("/dev/RPI_GPIO_3", O_RDWR | O_SYNC | O_CLOEXEC);
+  
+  if(fd < 0)
+  {
+    printf("ERROR opening RPI_GPIO device\n");
+    return -1;
+  }
+  
+  gpio =(uint32_t *)mmap(0, BLOCK_SIZE, PROT_READ|PROT_WRITE,
+                         MAP_SHARED, fd, GPIO_BASE);
+  
+  if(gpio == MAP_FAILED)
+  {
+    printf("ERROR mmaping into RPI_GPIO device\n");
+    return -2;
+  }
+  
+  //close(fd);
+  
+  return 0;
+}
